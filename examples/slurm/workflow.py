4123266fb3143578b9cf2013a3ee2df72b37366d
Arne Ludwig <arne.ludwig@posteo.de>
2022-05-09 21:32:18 +0200

    Minor refactoring of example

--- examples/slurm/workflow.py
+++ examples/slurm/workflow.py
@@ -1,5 +1,6 @@
 from dentist import *
 from dentist.workflow.engine.workflow import Job
+from itertools import chain
 from pathlib import Path
 
 
@@ -9,10 +10,9 @@ def example_workflow(*, count, outdir):
     # make sure outdir exists
     outdir.mkdir(parents=True, exist_ok=True)
 
-    generated_files = list()
     for i in range(count):
-        job = workflow.collect_job(
-            name=f"generate_{i}",
+        workflow.collect_job(
+            name="generate",
             inputs=[],
             outputs=[outdir / f"file_{i}"],
             action=lambda: ShellScript(
@@ -20,13 +20,11 @@ def example_workflow(*, count, outdir):
                 ShellCommand(["echo", f"data-{i:05d}"], stdout=outputs[0]),
             ),
         )
-        generated_files.extend(job.outputs)
-
     workflow.execute_jobs()
 
     workflow.collect_job(
         name="concat_results",
-        inputs=generated_files,
+        inputs=chain.from_iterable(job.outputs for job in workflow.jobs["generate"]),
         outputs=[outdir / "combined.out"],
         action=lambda: ShellScript(ShellCommand(["cat", *inputs], stdout=outputs[0])),
     )
