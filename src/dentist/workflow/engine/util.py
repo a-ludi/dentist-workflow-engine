68e7aa37032de1c0b011f8ddca06b345776549c7
Arne Ludwig <arne.ludwig@posteo.de>
2022-05-14 08:07:37 +0200

    Added `util.inject` and unit tests

---
+++ src/dentist/workflow/engine/util.py
@@ -0,0 +1,24 @@
+import inspect
+
+
+def inject(function, required=[], **vars):
+    params = inspect.signature(function).parameters
+
+    if any(p.kind == inspect.Parameter.VAR_KEYWORD for p in params.values()):
+        # function takes any number of keyword arguments -> pass all vars
+        injected_vars = vars
+    else:
+        injected_vars = dict()
+        for key, value in vars.items():
+            if key in params:
+                injected_vars[key] = value
+            elif key in required:
+                raise ValueError(f"missing required argument `{key}`")
+
+    def injected(**kwargs):
+        new_kw = {**injected_vars, **kwargs}
+        return function(**new_kw)
+
+    injected.__name__ = function.__name__
+
+    return injected
---
+++ tests/dentist/workflow/engine/test_util.py
@@ -0,0 +1,38 @@
+from dentist.workflow.engine.util import inject
+
+
+def test_inject():
+    vars = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+    }
+    test_required = {"a"}
+
+    def fun1(a):
+        return a == vars["a"]
+
+    def fun2(a, b):
+        return a == vars["a"] and b == vars["b"]
+
+    def fun3(b, c):
+        return b == vars["b"] and c == vars["c"]
+
+    # test average use case
+    assert inject(fun1, **vars)() is True
+    assert inject(fun2, **vars)() is True
+    assert inject(fun3, **vars)() is True
+
+    # test required parameter
+    assert inject(fun1, required=test_required, **vars)() is True
+    assert inject(fun2, required=test_required, **vars)() is True
+    try:
+        inject(fun3, required=test_required, **vars)
+        assert False
+    except ValueError:
+        assert True
+
+    # test additional args in secondary call
+    assert inject(fun1)(a=vars["a"]) is True
+    assert inject(fun2, a=vars["a"])(b=vars["b"]) is True
+    assert inject(fun3, c=vars["c"])(b=vars["b"]) is True
